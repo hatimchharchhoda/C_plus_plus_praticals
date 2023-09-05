8bfa9f5c694ad4ce1f6cfd290cb6c8657e34990b
hatimchharchhoda <140977639+hatimchharchhoda@users.noreply.github.com>
2023-09-05 23:13:58 +0530

    Update class_objectvalue_addvalue_3rdobject.cpp

--- class_objectvalue_addvalue_3rdobject.cpp
+++ class_objectvalue_addvalue_3rdobject.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 using namespace std;
 class object2;
-class object3;
+class object;
+class object3
+{
+  int c;
+  public:
+      void sum(object,object2);
+
+;
 class object{
   int a;
   public:
       object(int g)
       {
          a=g;
+         cout<<"value of 1st integer: "<<a<<endl;
       }
-       friend void object3::sum(object o1(g),object2 o(k));
+       friend void object3::sum(object,object2);
 };
 class object2{
    int b;
@@ -17,22 +25,18 @@ class object2{
        object2(int g)
        {
          b=g;
+         cout<<"value of 2nd integer: "<<b<<endl;
        }
-       friend void object3::sum(object o1(g),object2 o(k));
+       friend void object3::sum(object,object2);
 };
-class object3
-{
-  int c;
-  public:
-      void sum(object o1(g),object2 o(k))
-      {
+ void object3::sum(object o1,object2 o)
+         {
          cout<<"sum of no. is: "<<o1.a+o.b;
        }
-};
 int main()
 {
-object o(5);//
-object o1(10);
+object o(5);
+object2 o1(10);
 object3 oo;
 oo.sum(o,o1);
 return 0;
