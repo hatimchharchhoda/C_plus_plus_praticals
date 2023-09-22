4833634d37a8eca64cdcc0c0ccf7c3f62a307087
hatimchharchhoda <140977639+hatimchharchhoda@users.noreply.github.com>
2023-09-22 11:36:31 +0530

    Update hamming_code.cpp

--- hamming_code.cpp
+++ hamming_code.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string.h>
 using namespace std;
 int main()
 {
@@ -13,52 +12,46 @@ int main()
     cout<<endl<<"Codeword is : "<<a<<r2<<r1<<r0<<endl;
 
 
-    char b[100],q[10];
+
+    char b[100];
     int s0,s1,s2;
-    cout<<"Enter Codeword: "<<endl;
+    cout<<"\nEnter Codeword: ";
     cin>>b;
     s0=(b[3]+b[2]+b[1]+b[6])%2;
     s1=(b[2]+b[1]+b[0]+b[5])%2;
     s2=(b[3]+b[2]+b[0]+b[4])%2;
-    cout<<s2<<s1<<s0<<endl;
-    cout<<"Enter the above code: ";
-    cin>>q[19];
+    cout<<"error code: "<<s2<<s1<<s0<<endl;
     cout<<endl<<endl;
-    if(s2==0 && s1==0 && s0==0)
+    if(s2==0 && s1==1 && s0==1)
     {
-        cout<<"Dataword is: ";
-        for(int i=0; i<4; i++)
-        {
-            cout<<b[i];
-        }
+        if(b[1]=='0')
+            b[1]='1';
+        else
+            b[1]='0';
     }
-    switch (q[10]){
-         case '011' : if(b[1]==0)
-                        b[1]=1;
-                      else
-                        b[1]=0;
-                      break;
-        case '101' : if(b[3]==0)
-                        b[3]=1;
-                      else
-                        b[3]=0;
-                      break;
-        case '110' : if(b[0]==0)
-                        b[0]=1;
-                      else
-                        b[0]=0;
-                      break;
-        case '111' : if(b[2]==0)
-                        b[2]=1;
-                      else
-                        b[2]=0;
-                      break;
-        default : break;
+    if(s2==1 && s1==0 && s0==1)
+    {
+        if(b[3]=='0')
+            b[3]='1';
+        else
+            b[3]='0';
     }
-          cout<<"Dataword is: ";
-        for(int i=0; i<4; i++)
-        {
-            cout<<b[i];
-        }
+    if(s2==1 && s1==1 && s0==0)
+    {
+        if(b[0]=='0')
+            b[0]='1';
+        else
+            b[0]='0';
+    }
+    if(s2==1 && s1==1 && s0==1)
+    {
+        if(b[2]=='0')
+            b[2]='1';
+        else
+            b[2]='0';
+    }
+    cout<<"\n Dataword is: ";
+    for(int i=0; i<4; i++)
+        cout<<b[i];
     return 0;
 }
